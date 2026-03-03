57c4a8c7d0ec9f28b69bd194be28c951c698eb82
cplepage <lepage.charlesphilippe@gmail.com>
2026-03-03 17:07:20 -0500

    tailwindcss command

---
+++ cli/tailwindcss.ts
@@ -0,0 +1,9 @@
+import { Command } from "./types";
+
+export const tailwindcss: Command = {
+    name: "tailwindcss",
+    description: "Stub for tailwindcss (compilation handled externally)",
+    execute: async () => {
+        return 0;
+    }
+};
