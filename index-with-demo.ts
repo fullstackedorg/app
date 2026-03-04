447c8035f1c0bec8d74ef6a5b39978229c5a999f
cplepage <lepage.charlesphilippe@gmail.com>
2026-03-04 16:14:22 -0500

    skip welcome logic

--- index-with-demo.ts
+++ index-with-demo.ts
@@ -1,7 +1,34 @@
-if (location.search.includes("term")) {
-    await import("./terminal");
-} else {
-    await import("./demo");
+import fs from "fs";
+
+const skipFile = ".skip-welcome";
+
+const saveLastSeen = (skipWelcomeForever: boolean) => {
+    const cachedLastSeen = skipWelcomeForever
+        ? Number.MAX_SAFE_INTEGER
+        : Date.now();
+
+    fs.promises.writeFile(skipFile, cachedLastSeen.toString());
 }
 
-export {};
+const openTerminal = async () => {
+    await import("./terminal");
+}
+
+const getLastSeen = async () => {
+    try {
+        return parseInt(await fs.promises.readFile(skipFile, { encoding: "utf-8" }));
+    } catch (e) { return 0 }
+}
+
+if (Date.now() - await getLastSeen() < 1000 * 60 * 24) { // 24h
+    openTerminal();
+} else {
+    const showWelcomeMessage = (await import("./demo/init")).default;
+    showWelcomeMessage((dontShowAgain) => {
+        openTerminal()
+        if (dontShowAgain) {
+            saveLastSeen(true)
+        }
+    });
+    saveLastSeen(false);
+}
